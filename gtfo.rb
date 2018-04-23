e3d74fd954f56fd29979882aa5f94b7f26738ea5
renzo <github@renzokuken.eu>
2018-04-23 11:34:01 +0200

    lol

--- gtfo.rb
+++ gtfo.rb
@@ -68,7 +68,7 @@ __END__
   <div class="row">
     <h3>CybeR || GTFO</h3>
     <h6><i>Some <a href="https://nostarch.com/gtfo">wannabe hackers</a> wanted to bring back old electronics&computers Zines feels. Unfortunately they forgot to include any kind of cyber in there. This is a free repo of proper cyber illustration.
-        <a href="https://github.com/conchyliculture/cyber-gtfo">Github</a></i></h6>
+        <a href="https://github.com/conchyliculture/cyber-gtfo">Github for this terrible mess of code</a></i></h6>
   </div>
   <div class="row">
 	<form action="/search" class="input-field inline col s4">
